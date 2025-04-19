0157f17f796959c02a4f57153bd34f92dd1fdf01
YusukeTakatsu <github ys.taka44@gmail.com>
2025-04-19 13:56:14 +0900

    fix:project name

--- base_locals.tf
+++ base_locals.tf
@@ -1,9 +1,9 @@
 locals {
-  project = "Training"
+  project = "training"
   region  = "ap-northeast-1"
   default_tags = {
     Managed     = "terraform"
-    Project     = local.project
+    Project     = "Training"
     Environment = local.env
   }
   github_repository_prefix = local.project
